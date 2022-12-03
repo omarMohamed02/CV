f0a4e1dedfc4dbc0112d32f3232c5d0768a3180b
Omar Kamal <omrkmal2828@gmail.com>
2022-12-03 22:21:38 +0300

    add dark-mode

--- app.js
+++ app.js
@@ -36,10 +36,54 @@ function CreatePDFfromHTML() {
   });
 }
 
+$(window).on("load", function () {
+  let mode = localStorage.getItem("mode");
+  $(".loader").hide();
+  if (!mode) {
+    localStorage.setItem("mode", "light");
+  } else {
+    if (mode == "dark") {
+      $("body").addClass("dark-mode");
+      $("#mode").attr("src", "light.svg");
+
+      $("#light").hide();
+      $("#dark").show();
+    } else {
+      $("body").removeClass("dark-mode");
+      $("#mode").attr("src", "moon.svg");
+      $("#dark").hide();
+      $("#light").show();
+    }
+  }
+});
+
 $("#downloadCV").on("click", function () {
   $("#downloadCV").hide();
   CreatePDFfromHTML();
   setTimeout(() => {
     $("#downloadCV").show();
   }, 500);
+});
+
+// function checkMode() {
+
+// }
+
+$(".mode").on("click", function () {
+  let mode = localStorage.getItem("mode");
+
+  if (mode == "dark") {
+    // Light mode
+    $("body").removeClass("dark-mode");
+    localStorage.setItem("mode", "light");
+    $("#mode").attr("src", "light.svg");
+    $("#dark").hide();
+    $("#light").show();
+  } else {
+    // Dark mode
+    $("body").addClass("dark-mode");
+    localStorage.setItem("mode", "dark");
+    $("#light").hide();
+    $("#dark").show();
+  }
 });
