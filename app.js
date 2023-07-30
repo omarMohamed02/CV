5cab9bbe25adcf45354734f3b923570123fb6b13
Omar Mohamed <omar.mohamed@zid.sa>
2023-07-30 22:47:50 +0300

    change profile image

--- app.js
+++ app.js
@@ -1,41 +1,3 @@
-//Create PDf from HTML...
-function CreatePDFfromHTML() {
-  var HTML_Width = $("body").width();
-  var HTML_Height = $("body").height();
-  var top_left_margin = 15;
-  var PDF_Width = HTML_Width + top_left_margin * 2;
-  var PDF_Height = PDF_Width * 1.5 + top_left_margin * 2;
-  var canvas_image_width = HTML_Width;
-  var canvas_image_height = HTML_Height;
-
-  var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
-
-  html2canvas($("body")[0]).then(function (canvas) {
-    var imgData = canvas.toDataURL("image/jpeg", 1.0);
-    var pdf = new jsPDF("p", "pt", [PDF_Width, PDF_Height]);
-    pdf.addImage(
-      imgData,
-      "JPG",
-      top_left_margin,
-      top_left_margin,
-      canvas_image_width,
-      canvas_image_height
-    );
-    for (var i = 1; i <= totalPDFPages; i++) {
-      pdf.addPage(PDF_Width, PDF_Height);
-      pdf.addImage(
-        imgData,
-        "JPG",
-        top_left_margin,
-        -(PDF_Height * i) + top_left_margin * 4,
-        canvas_image_width,
-        canvas_image_height
-      );
-    }
-    pdf.save("Your_PDF_Name.pdf");
-  });
-}
-
 $(window).on("load", function () {
   let mode = localStorage.getItem("mode");
   $(".loader").hide();
@@ -57,18 +19,6 @@ $(window).on("load", function () {
   }
 });
 
-$("#downloadCV").on("click", function () {
-  $("#downloadCV").hide();
-  CreatePDFfromHTML();
-  setTimeout(() => {
-    $("#downloadCV").show();
-  }, 500);
-});
-
-// function checkMode() {
-
-// }
-
 $(".mode").on("click", function () {
   let mode = localStorage.getItem("mode");
 
@@ -88,6 +38,6 @@ $(".mode").on("click", function () {
   }
 });
 
-document.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-});
+// document.addEventListener("contextmenu", (e) => {
+//   e.preventDefault();
+// });
